b79429b5ff683a11ddd9d130e5a4bf35daccd2a9
sulkhan <sulkhangalangsakti@gmail.com>
2023-07-12 05:51:29 +0700

    setup tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+  plugins: [vue({
+    template: {
+      compilerOptions: {
+        // treat all tags with a dash as custom elements
+        isCustomElement: (tag) => tag.includes('-')
+      }
     }
+  })],
+  build: {
+    lib: {
+      entry: './src/entry.js',
+      formats: ['es','cjs'],
+      name: 'web-component',
+      fileName: (format)=>(format === 'es' ? 'index.js' : 'index.cjs')
+    },
+    sourcemap: true,
+    target: 'esnext',
+    minify: false
   }
 })
